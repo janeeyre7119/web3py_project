cdae04a290575e9e734181c0dd58e6a894f94c30
Lisa Kelly <92017738+lisakelly271@users.noreply.github.com>
2023-06-15 14:18:01 +0700

    Update checkbalance.py

--- checkbalance.py
+++ checkbalance.py
@@ -5,21 +5,9 @@ from web3 import Web3
 sepolia_url ='https://rpc.sepolia.org'
 goerli_url = 'https://goerli.blockpi.network/v1/rpc/public'
 eth_url = 'https://1rpc.io/eth'
-base_url = 'https://goerli.base.org'
-scroll_url = 'https://alpha-rpc.scroll.io/l2'
-taiko_url = 'https://rpc.a2.taiko.xyz/'
-bsc_url = 'https://bsc-dataseed1.binance.org/'
-polygon_url = 'https://polygon-rpc.com'
-linea_rpc = 'https://rpc.goerli.linea.build'
-avax_testnet = 'https://rpc.ankr.com/avalanche_fuji'
-op = 'https://opt-mainnet.g.alchemy.com/v2/demo'
-
-#Smart contract
-token_address = '0x60e6895184448f3e8ef509d083e3cc3ac31f82fd'
-address = '0xC62DC5C6f306E67BF1e9D0346b579000Bc88b6c4'
 
 #Connection
-web3 = Web3(Web3.HTTPProvider(bsc_url))
+web3 = Web3(Web3.HTTPProvider(sepolia_url))
 print('connection', web3.is_connected())
 
 #check balance
